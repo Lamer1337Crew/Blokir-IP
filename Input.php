2ca8ff0bf9562a6ad5ea15387b93322cf6eaffa7
VanGans <41531599+Lamer1337Crew@users.noreply.github.com>
2019-02-19 18:00:13 +0700

    Create Input.php

---
+++ Input.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Blockir Ip</title>
+</head>
+<body>
+    <h1>Masukkan Yang Mau Di Blockir</h1>
+    <form method="post" action="aksi.php" enctype="multypart/form-data">
+        <table>
+            <tr>
+                <td colspan="2"><input type="text" name="target"></td>
+            </tr>
+            <tr>
+                <td><input type="submit" value="Kunci"></td>
+                <td><input type="reset" value="Batal"></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
